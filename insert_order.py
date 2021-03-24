4968d0aff9174562f61daeef6df1173339461eac
aorlander <aborlander@gmail.com>
2021-03-24 18:26:13 -0400

    import

--- insert_order.py
+++ insert_order.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from import create_engine
+from sqlalchemy.orm import 
 import random
 from models import Base, Order
 
