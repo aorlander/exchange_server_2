b4daac8ab8a4efa18a826f15fa448b36a4b0207d
aorlander <aborlander@gmail.com>
2021-03-26 12:30:57 -0400

    test

--- order_book.py
+++ order_book.py
@@ -28,17 +28,47 @@ def process_order(order):
     #      5) The implied exchange rate of the new order must be at least that of the existing order (existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount/order.sell_amount)
     #      6) The buy / sell amounts need not match exactly
     #      7) Each order should match at most one other
-    
-    for existing_order in session.query(Order).filter(Order.filled == None): 
-        if(existing_order.buy_currency == current_order.sell_currency):
-            if(existing_order.sell_currency == current_order.buy_currency):
-                if(existing_order.sell_amount / existing_order.buy_amount >= current_order.buy_amount/current_order.sell_amount):
-                    #print("current " + current_order.sender_pk + " matched with existing "+ existing_order.sender_pk)
-                    existing_order.filled = current_order.timestamp #or does this mean NOW?
-                    current_order.filled = current_order.timestamp
-                    existing_order.counterparty_id = current_order.id
-                    current_order.counterparty_id = existing_order.id
-                    if (existing_order.sell_amount < current_order.buy_amount):
+    print("\n current: SELL " + str(current_order.sell_amount) + " " + current_order.sell_currency + " / BUY " + str(current_order.buy_amount) + " " + current_order.buy_currency)
+
+    for existing_order in session.query(Order).filter(Order.filled == None):
+        if(current_order.filled==None): 
+            if(existing_order.buy_currency == current_order.sell_currency):
+                if(existing_order.sell_currency == current_order.buy_currency):
+                    if(existing_order.sell_amount / existing_order.buy_amount >= current_order.buy_amount/current_order.sell_amount):
+                        if (existing_order.sell_amount < current_order.buy_amount):
+                            print("existing SELL " + str(existing_order.sell_amount) + " " + existing_order.sell_currency + " / current BUY " + str(current_order.buy_amount) + " " + current_order.buy_currency)
+                            print("existing BUY " + str(existing_order.buy_amount) + " " + existing_order.buy_currency + " / current SELL " + str(current_order.sell_amount) + " " + current_order.sell_currency)
+                            remaining_sell_amount = current_order.sell_amount - existing_order.buy_amount
+                            remaining_buy_amount = current_order.buy_amount - existing_order.sell_amount
+                            child_order = Order (
+                                creator_id=current_order.id, 
+                                sender_pk=current_order.sender_pk,
+                                receiver_pk=current_order.receiver_pk, 
+                                buy_currency=current_order.buy_currency, 
+                                sell_currency=current_order.sell_currency, 
+                                buy_amount=remaining_buy_amount, 
+                                sell_amount=remaining_sell_amount )
+                            child_order.timestamp = datetime.now()
+                            child_order.relationship = (child_order.id, current_order.id)
+                            current_order.relationship = (child_order.id, current_order.id)
+                            session.add(child_order)
+                            session.commit()
+                            print("created: SELL " + str(child_order.sell_amount) + " " + child_order.sell_currency + " / BUY " + str(child_order.buy_amount) + " " + child_order.buy_currency)
+
+                            existing_order.counterparty_id = current_order.id
+                            existing_order.filled = current_order.timestamp #or does this mean NOW?
+                            current_order.counterparty_id = existing_order.id
+                            current_order.filled = current_order.timestamp
+
+    print("ALL: ")
+    for o in session.query(Order):
+        print(str(o.id) + ": SELL " + str(o.sell_amount) + " " + o.sell_currency + " / BUY " + str(o.buy_amount) + " " + o.buy_currency + " ( " + str(o.creator_id) + " )")
+    print("FILLED: ")
+    for o in session.query(Order).filter(Order.filled != None):
+        print(str(o.id) + ": SELL " + str(o.sell_amount) + " " + o.sell_currency + " / BUY " + str(o.buy_amount) + " " + o.buy_currency + " ( " + str(o.creator_id) + " )")
+
+                        
+                        
 
     #3. If a match is found between order and existing_order:
     #      – Set the filled field to be the current timestamp on both orders
@@ -53,18 +83,6 @@ def process_order(order):
     # -Each order matches at most one other (to match one order against multiple others create derivative orders, and set the “created_by” field as described above)
     # -Any derived orders must have an implied exchange rate that is at least the original exchange rate, i.e., buy_amount/sell_amount on the new order must be at least the buy_amount/sell_amount on the order that created it
 
-                        print("create new order:")
-                        remaining_balance = current_order.buy_amount - existing_order.sell_amount
-                        child_order = Order (creator_id=current_order.id, 
-                            sender_pk=current_order.sender_pk,
-                            receiver_pk=current_order.receiver_pk, 
-                            buy_currency=order['buy_currency'], 
-                            sell_currency=order['sell_currency'], 
-                            buy_amount=remaining_balance, 
-                            sell_amount=0 )
-                        session.add(child_order)
-                        session.commit()
-                    break
 
     #DB should provide a complete history of all orders submitted and filled - do not delete filled orders; create derived orders with creator_id
     
