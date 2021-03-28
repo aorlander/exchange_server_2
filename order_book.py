966bd161457b7e0d378dc045788826624c2c2110
aorlander <aborlander@gmail.com>
2021-03-28 09:47:31 -0400

    clean up

--- order_book.py
+++ order_book.py
@@ -79,12 +79,12 @@ def payouts_from_exchange():
     return 0
 
 # Check if there are any existing orders that match. Given new_order and existing order, to match all of the following requirements must be fulfilled:
-#      1) existing_order.filled must be None
-#      2) existing_order.buy_currency == order.sell_currency
-#      3) existing_order.sell_currency == order.buy_currency
-#      4) The implied exchange rate of the new order must be at least that of the existing order 
-#      5) The buy / sell amounts need not match exactly
-#      6) Each order should match at most one other
+#   1) existing_order.filled must be None
+#   2) existing_order.buy_currency == order.sell_currency
+#   3) existing_order.sell_currency == order.buy_currency
+#   4) The implied exchange rate of the new order must be at least that of the existing order 
+#   5) The buy / sell amounts need not match exactly
+#   6) Each order should match at most one other
 def check_match(existing_order, order):
     if(order.filled==None):
         if(existing_order.buy_currency == order.sell_currency):
@@ -93,16 +93,15 @@ def check_match(existing_order, order):
                      return True
     return False
 
-
 # If a match is found between order and existing_order:
-#      – Set the filled field to be the current timestamp on both orders
-#      – Set counterparty_id to be the id of the other order
-#      – If one of the orders is not completely filled (i.e. the counterparty’s sell_amount is less than buy_amount):
-#            -Create a new order for remaining balance
-#            -The new order should have the created_by field set to the id of its parent order
-#            -The new order should have the same pk and platform as its parent order
-#            -The sell_amount of the new order can be any value such that the implied exchange rate of the new order is at least that of the old order (i.e., B/S on the new order must be at least the B/S on the order that created it)
-#            -You can then try to fill the new order
+#  – Set the filled field to be the current timestamp on both orders
+#  – Set counterparty_id to be the id of the other order
+#  – If one of the orders is not completely filled (i.e. the counterparty’s sell_amount is less than buy_amount):
+#     -Create a new order for remaining balance
+#     -The new order should have the created_by field set to the id of its parent order
+#     -The new order should have the same pk and platform as its parent order
+#     -The sell_amount of the new order can be any value such that the implied exchange rate of the new order is at least that of the old order (i.e., B/S on the new order must be at least the B/S on the order that created it)
+#     -You can then try to fill the new order
 def match_order(existing_order, order):
     existing_order.filled = order.timestamp 
     order.filled = order.timestamp
@@ -131,8 +130,7 @@ def match_order(existing_order, order):
         #print("created: SELL " + str(child_order.sell_amount) + " " + child_order.sell_currency + " / BUY " + str(child_order.buy_amount) + " " + child_order.buy_currency)
     return 0
 
-
-#Your solution should be in a file called ‘order_book.py’ and it should contain a method 'process_order(order)'. The function ‘process_order’ takes a single argument, a dictionary object containing the 6 fields above.
+# The function ‘process_order’ takes a single argument, a dictionary object containing the 6 fields above.
 def process_order(order):
     #Insert the order into the database. 
     current_order = Order( sender_pk=order['sender_pk'], receiver_pk=order['receiver_pk'], buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], buy_amount=order['buy_amount'], sell_amount=order['sell_amount'])
@@ -143,5 +141,4 @@ def process_order(order):
     for existing_order in session.query(Order).filter(Order.filled == None):
         if(check_match(existing_order, current_order)==True):
             match_order(existing_order, current_order)
-
     pass
