a24de04e010638e1654eb1f80ea3388922795810
aorlander <aborlander@gmail.com>
2021-03-28 15:58:59 -0400

    test

--- test_orders.py
+++ test_orders.py
@@ -29,7 +29,7 @@ def make_order(platform):
 from order_book import process_order
 
 order_list = [] 
-num_orders=20
+num_orders=10
 bar = progressbar.ProgressBar(max_value=num_orders, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 for i in range(num_orders):
     bar.update(i+1)
@@ -40,14 +40,8 @@ for i in range(num_orders):
 bar.finish()
 
 
-
-from order_book import calc_net_deposits
 from order_book import calc_net_profits
-from order_book import payouts_from_exchange
 from order_book import list_orders
-
-#calc_net_deposits()
-#payouts_from_exchange()
 
 list_orders()
 calc_net_profits()
